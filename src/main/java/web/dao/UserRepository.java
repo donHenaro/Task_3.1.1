afcb97e611bde0aba285a1103fa64c3d4fc34349
Андрей <mgb@tut.by>
2021-08-25 00:13:52 +0300

    fixup! JpaRepository + Корректировка пароля при update

---
+++ src/main/java/web/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package web.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import web.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
